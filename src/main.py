084f37a14efb5fed8a734ddac88a316f488c4fd0
Claire Randolph <claire.randolph@hotmail.com>
2026-02-04 09:38:45 -0500

    Added avg_sortie_dur and id to SquadronConfig

--- src/main.py
+++ src/main.py
@@ -5,22 +5,20 @@ if __name__ == "__main__":
     cfg = SquadronConfig(
         ute=10,
         paa=18,
+        id=99,
         mqt_students=5,
         flug_students=3,
         ipug_students=3,
         total_pilots=30,
         experience_ratio=0.5,
         ip_qty=4,
-        phase_length_days=120
+        phase_length_days=120,
+        avg_sortie_dur = 1.3
     )
 
-    # Create pilots (you might want to move pilot creation out of run_phase 
-    # if you want to persist them across multiple phases later)
     from engine import create_pilots
     pilots = create_pilots(cfg)
 
-    # Run
     run_phase_simulation(cfg, pilots)
     
-    # Report
     print_phase_summary(pilots, cfg, verbose=False)
