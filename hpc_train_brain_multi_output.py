9780ec45636570c330c994c3494f79799b2bc36d
Claire Randolph <claire.randolph@hotmail.com>
2026-05-08 13:54:30 -0400

    Cleaned up code base and added arguments to training function

--- hpc_train_brain_multi_output.py
+++ hpc_train_brain_multi_output.py
@@ -4,6 +4,8 @@ import numpy as np
 import joblib
 import glob
 import os
+from enum import Enum
+
 from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.model_selection import train_test_split
@@ -14,12 +16,14 @@ from sklearn.metrics import r2_score
 from sklearn.neural_network import MLPRegressor
 
 INPUT_DIR = "outputs/single_phase/repart_parquet"
-# OUTPUT_MODEL = "outputs/single_phase/brains/hpc_sortie_brain_multi_output_hybrid.pkl" 
-OUTPUT_MODEL = "outputs/single_phase/brains/hpc_sortie_brain_multi_output_mlp.pkl" 
 SAMPLE_FRAC = 0.10 
 RANDOM_SEED = 42
 
-def train_hpc_multi_brain():
+class Regressor(Enum):
+    MLP = 'MLP'
+    HYB = 'HYB'
+
+def train_hpc_multi_brain(regressor: Regressor, raw_data:bool):
     print(f"🚀 Starting Multi-Output HPC Brain Training...")
     files = glob.glob(os.path.join(INPUT_DIR, "part.*.parquet"))
     
@@ -27,7 +31,7 @@ def train_hpc_multi_brain():
         print(f"❌ No parquet files found in {INPUT_DIR}")
         return
 
-    # 1. LOAD & SAMPLE DATA
+    # LOAD & SAMPLE DATA
     mini_batches = []
     for f in files:
         df_chunk = pd.read_parquet(f)
@@ -38,18 +42,20 @@ def train_hpc_multi_brain():
     df = pd.concat(mini_batches, ignore_index=True)
     print(f"📊 Dataset loaded: {len(df):,} rows")
 
-    # 2. FEATURE & TARGET SELECTION
+    # FEATURE & TARGET SELECTION
     base_features = ['paa', 'ute', 'exp_ratio', 'total_pilots', 'mqt_qty', 'flug_qty', 'ipug_qty', 'wg_qty', 'fl_qty','ip_qty']
     for col in base_features:
         if col not in df.columns: 
             df[col] = 0
-    ips = df['ip_qty'].replace(0, 0.5)
+
+    ips = df['ip_qty'].replace(0, 1.0)
     fls = df['fl_qty'].replace(0, 1.0)
     wgs = df['wg_qty'].replace(0, 1.0)
 
-    df['mqt_load'] = df['mqt_qty'] / ips
-    df['flug_load'] = df['flug_qty'] / ips
-    df['ipug_load'] = df['ipug_qty'] / ips
+    if raw_data:
+        df['mqt_load'] = df['mqt_qty'] / ips
+        df['flug_load'] = df['flug_qty'] / ips
+        df['ipug_load'] = df['ipug_qty'] / ips
     
     df['fl_congestion'] = (df['ipug_qty'] + df['flug_qty']) / fls
     df['wg_crowding'] = (df['mqt_qty'] + df['flug_qty'] + df['ipug_qty']) / wgs
@@ -62,11 +68,19 @@ def train_hpc_multi_brain():
     df['ip_to_stud_ratio'] = df['ip_qty'] / df['total_students'].replace(0, 0.1)
     
     df = df.replace([np.inf, -np.inf], 0)
-    features = [
-        # 'paa', 'ute', 'mqt_load', 'flug_load', 'ipug_load', # Add next HPC run
-        'exp_ratio', 'ip_ratio', 'fl_congestion',
-        'wg_crowding', 'sorties_avail', 'pilot_to_sortie', 'ip_to_stud_ratio'
-    ]
+    
+    if raw_data:
+        features = [
+            'paa', 'ute', 'mqt_load', 'flug_load', 'ipug_load', 
+            'exp_ratio', 'ip_ratio', 'fl_congestion',
+            'wg_crowding', 'sorties_avail', 'pilot_to_sortie', 'ip_to_stud_ratio'
+        ]
+           
+    else:
+        features = [
+            'exp_ratio', 'ip_ratio', 'fl_congestion',
+            'wg_crowding', 'sorties_avail', 'pilot_to_sortie', 'ip_to_stud_ratio'
+        ]
     
     targets = [
         'wg_monthly', 'fl_monthly', 'ip_monthly', 
@@ -79,83 +93,97 @@ def train_hpc_multi_brain():
     # 3. SPLIT
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=RANDOM_SEED)
 
-    # # 4. DEFINE HYBRID MODEL
-    # base_model = Pipeline([
-    #     ('scaler', StandardScaler()),
-    #     ('ridge', Ridge(alpha=1.0))
-    # ])
-    # linear_model = MultiOutputRegressor(base_model)
-    # print("🧠 Training Linear model...")
-    # linear_model.fit(X_train, Y_train)
+    if regressor.name == 'HYB':
+        OUTPUT_MODEL = "outputs/single_phase/brains/hpc_sortie_brain_multi_output_hybrid.pkl" 
+        
+        # DEFINE MODEL
+        base_model = Pipeline([
+            ('scaler', StandardScaler()),
+            ('ridge', Ridge(alpha=1.0))
+        ])
+        linear_model = MultiOutputRegressor(base_model)
+        print("🧠 Training Linear model...")
+        linear_model.fit(X_train, Y_train)
 
-    # wg_coefs = linear_model.estimators_[0].named_steps['ridge'].coef_
-    # print(f"WG Ridge Coefs: {wg_coefs}")    
-    # fl_coefs = linear_model.estimators_[1].named_steps['ridge'].coef_
-    # print(f"FL Ridge Coefs: {fl_coefs}")
-    # ip_coefs = linear_model.estimators_[2].named_steps['ridge'].coef_
-    # print(f"IP Ridge Coefs: {ip_coefs}")
+        wg_coefs = linear_model.estimators_[0].named_steps['ridge'].coef_
+        print(f"WG Ridge Coefs: {wg_coefs}")    
+        fl_coefs = linear_model.estimators_[1].named_steps['ridge'].coef_
+        print(f"FL Ridge Coefs: {fl_coefs}")
+        ip_coefs = linear_model.estimators_[2].named_steps['ridge'].coef_
+        print(f"IP Ridge Coefs: {ip_coefs}")
 
-    # Y_train_pred_lin = linear_model.predict(X_train)
+        Y_train_pred_lin = linear_model.predict(X_train)
 
-    # print("🧮 Calculating Residuals...")
-    # residuals = Y_train - Y_train_pred_lin
+        print("🧮 Calculating Residuals...")
+        residuals = Y_train - Y_train_pred_lin
 
-    # booster_model = MultiOutputRegressor(
-    #     HistGradientBoostingRegressor(
-    #     max_iter=150,
-    #     learning_rate=0.03,     
-    #     max_leaf_nodes=10,      
-    #     min_samples_leaf=1000,   
-    #     l2_regularization=50.0,  
-    #     random_state=RANDOM_SEED
-    #     )
-    # )
-    # print("🧠 Training Booster model...")
-    # booster_model.fit(X_train, residuals)
+        booster_model = MultiOutputRegressor(
+            HistGradientBoostingRegressor(
+            max_iter=150,
+            learning_rate=0.03,     
+            max_leaf_nodes=10,      
+            min_samples_leaf=1000,   
+            l2_regularization=50.0,  
+            random_state=RANDOM_SEED
+            )
+        )
 
-    # # 5. EVALUATE
-    # print("Models trained! Evaluating performance...")
-    # y_pred_lin_test = linear_model.predict(X_test)
-    # y_pred_res_test = booster_model.predict(X_test)
-    # y_pred_total = y_pred_lin_test + y_pred_res_test
+        # TRAIN
+        print("🧠 Training Booster model...")
+        booster_model.fit(X_train, residuals)
 
-    brain = MLPRegressor(
-        hidden_layer_sizes=(64, 64),
-        activation='relu',       # Critical for learning 'hinges' and floors
-        solver='adam',           # Standard optimizer for large datasets
-        alpha=0.001,             # L2 Regularization (prevents the 'wiggles')
-        batch_size=1024,         # Helps with the 50M row memory load
-        learning_rate_init=0.001,
-        max_iter=500,
-        early_stopping=True,     # Stops once it stops improving on the test set
-        random_state=42
-    )
-    mlp_model = Pipeline([
-        ('scaler', StandardScaler()),
-        ('mlp', brain)
-    ])
+        # EVALUATE
+        print("Models trained! Evaluating performance...")
+        y_pred_lin_test = linear_model.predict(X_test)
+        y_pred_res_test = booster_model.predict(X_test)
+        y_pred = y_pred_lin_test + y_pred_res_test
 
-    print("🧠 Training MLP Regressor model...")
-    mlp_model.fit(X_train, Y_train)
-    print("MLP Model trained! Evaluating performance...")
-    y_pred = mlp_model.predict(X_test)
+    elif regressor.name == 'MLP':
+        OUTPUT_MODEL = "outputs/single_phase/brains/hpc_sortie_brain_multi_output_mlp.pkl" 
+        
+        # DEFINE MODEL
+        brain = MLPRegressor(
+            hidden_layer_sizes=(64, 64),
+            activation='relu',       # Critical for learning 'hinges' and floors
+            solver='adam',           # Standard optimizer for large datasets
+            alpha=0.001,             # L2 Regularization (prevents the 'wiggles')
+            batch_size=1024,         # Helps with the 50M row memory load
+            learning_rate_init=0.001,
+            max_iter=500,
+            early_stopping=True,     # Stops once it stops improving on the test set
+            random_state=42
+        )
+        mlp_model = Pipeline([
+            ('scaler', StandardScaler()),
+            ('mlp', brain)
+        ])
 
+        # TRAIN
+        print("🧠 Training MLP Regressor model...")
+        mlp_model.fit(X_train, Y_train)
 
+        # EVALUATE
+        print("MLP Model trained! Evaluating performance...")
+        y_pred = mlp_model.predict(X_test)
+
+    # SCORE
     score = r2_score(Y_test, y_pred)
-    # score = r2_score(Y_test, y_pred_total)
     print(f"✅ Training Complete. Overall R² Score: {score:.4f}")
 
-    # 6. SAVE
+    # SAVE
     os.makedirs(os.path.dirname(OUTPUT_MODEL), exist_ok=True)
-    # hybrid_brain = {
-    #     'linear': linear_model,
-    #     'booster': booster_model
-    # }
-    # joblib.dump(hybrid_brain, OUTPUT_MODEL)
-    # print(f"💾 Hybrid brain saved to {OUTPUT_MODEL}")
-    MLP_brain = mlp_model
-    joblib.dump(mlp_model, OUTPUT_MODEL)
-    print(f"💾 MLP brain saved to {OUTPUT_MODEL}")
+    
+    if regressor.name == 'HYB':
+        hybrid_brain = {
+            'linear': linear_model,
+            'booster': booster_model
+        }
+        joblib.dump(hybrid_brain, OUTPUT_MODEL)
+        print(f"💾 Hybrid brain saved to {OUTPUT_MODEL}")
+
+    elif regressor.name == 'MLP':
+        joblib.dump(mlp_model, OUTPUT_MODEL)
+        print(f"💾 MLP brain saved to {OUTPUT_MODEL}")
 
 if __name__ == "__main__":
-    train_hpc_multi_brain()
+    train_hpc_multi_brain(regressor=Regressor.MLP, raw_data=False)
