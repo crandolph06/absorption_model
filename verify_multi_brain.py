fe4032ccc4d370219fa3e4c18c1632f4fa805bea
Claire Randolph <claire.randolph@hotmail.com>
2026-05-08 12:41:20 -0400

    Updated brain verifier with new features and ratios

--- verify_multi_brain.py
+++ verify_multi_brain.py
@@ -25,16 +25,35 @@ def verify_model():
     df = pd.read_parquet(files[0])
     
     # Pre-process exactly as we did in training
-    base_features = ['paa', 'ute', 'exp_ratio', 'total_pilots', 'mqt_qty', 'flug_qty', 'ipug_qty', 'ip_qty']
+    base_features = ['paa', 'ute', 'exp_ratio', 'total_pilots', 'mqt_qty', 'flug_qty', 'ipug_qty', 'wg_qty', 'fl_qty','ip_qty']
     for col in base_features:
-        if col not in df.columns: df[col] = 0
-            
+        if col not in df.columns: 
+            df[col] = 0
+    ips = df['ip_qty'].replace(0, 0.5)
+    fls = df['fl_qty'].replace(0, 1.0)
+    wgs = df['wg_qty'].replace(0, 1.0)
+
+    df['mqt_load'] = df['mqt_qty'] / ips
+    df['flug_load'] = df['flug_qty'] / ips
+    df['ipug_load'] = df['ipug_qty'] / ips
+    
+    df['fl_congestion'] = (df['ipug_qty'] + df['flug_qty']) / fls
+    df['wg_crowding'] = (df['mqt_qty'] + df['flug_qty'] + df['ipug_qty']) / wgs
+
+    df['sorties_avail'] = df['paa'] * df['ute']
+    df['pilot_to_sortie'] = df['total_pilots'] / df['sorties_avail']
+
     df['total_students'] = df['mqt_qty'] + df['flug_qty'] + df['ipug_qty']
     df['ip_ratio'] = df['ip_qty'] / df['total_pilots'].replace(0, 1)
     df['ip_to_stud_ratio'] = df['ip_qty'] / df['total_students'].replace(0, 0.1)
-    df = df.replace([np.inf, -np.inf], 0)
     
-    features = base_features + ['ip_ratio', 'ip_to_stud_ratio']
+    df = df.replace([np.inf, -np.inf], 0)
+    features = [
+        'exp_ratio', 'ip_ratio', 'fl_congestion',
+        'wg_crowding', 'sorties_avail', 'pilot_to_sortie', 'ip_to_stud_ratio'
+        # 'mqt_load', 'flug_load', 'ipug_load', 
+    ]
+    
     targets = ['wg_monthly', 'fl_monthly', 'ip_monthly', 
                'wg_blue_monthly', 'fl_blue_monthly', 'ip_blue_monthly']
     
