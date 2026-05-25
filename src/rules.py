8a8a01d258ba77d9d9c2ff9bcebb7bde9d760a12
Claire Randolph <claire.randolph@hotmail.com>
2026-05-25 15:06:22 -0400

    Corrected bad can_fill_seat logic

--- src/rules.py
+++ src/rules.py
@@ -1,8 +1,6 @@
 from src.models import Pilot, Qual, Upgrade
 
-# ---------------------------------------------------------
-# 1. ELIGIBILITY RULES (Used during "Selection Phase")
-# ---------------------------------------------------------
+
 def can_start_upgrade(pilot: Pilot, upgrade_type: Upgrade) -> bool:
     """
     Determines if a pilot is eligible to BEGIN a specific upgrade.
@@ -20,42 +18,30 @@ def can_start_upgrade(pilot: Pilot, upgrade_type: Upgrade) -> bool:
 
     return False
 
-# ---------------------------------------------------------
-# 2. SEAT RULES (Used during "Sortie Generation Phase")
-# ---------------------------------------------------------
-def _qual_hierarchy_check(pilot_qual: Qual, seat_required: Qual) -> bool:
-    """
-    Internal helper: Returns True if pilot rank >= seat rank.
-    Replaces: qual_meets_requirement
-    """
-    # IP can fly everything
-    if pilot_qual == Qual.IP:
-        return True
-    
-    # FL can fly FL and WG seats
-    if pilot_qual == Qual.FL:
-        return seat_required in [Qual.FL, Qual.WG]
-    
-    # WG can only fly WG seats
-    if pilot_qual == Qual.WG:
-        return seat_required == Qual.WG
-        
-    return False
-
-def can_fill_seat(pilot: Pilot, seat_type: Qual, syllabus_upgrade: Upgrade = None) -> bool:
+def can_fill_seat(pilot: Pilot, min_qual: Qual) -> bool:
     """
     Master rule: Can this pilot sit in this seat for this specific syllabus event?
     """
-    # 1. UPGRADE RESTRICTIONS (The "Lock-in" Rule)
-    # If pilot is in MQT, they cannot fly FLUG/IPUG/CT sorties, only MQT.
-    if pilot.upgrade == Upgrade.MQT and syllabus_upgrade != Upgrade.MQT:
-        return False
-    # If pilot is in FLUG, they cannot fly MQT sorties.
-    if pilot.upgrade == Upgrade.FLUG and syllabus_upgrade not in [Upgrade.FLUG, None]:
-        return False
-    # If pilot is in IPUG, they cannot fly other upgrades.
-    if pilot.upgrade == Upgrade.IPUG and syllabus_upgrade not in [Upgrade.IPUG, None]:
+    # Backup -- no MQT flying anything other than MQT upgrade sorties.
+    if pilot.upgrade == Upgrade.MQT:
         return False
 
-    # 2. HIERARCHY CHECK
-    return _qual_hierarchy_check(pilot.qual, seat_type)
+    # IP requirements
+    if min_qual == Qual.IP and pilot.qual == Qual.IP:
+        return True
+
+    # FLUG requirements
+    if min_qual == Qual.FL and pilot.qual == Qual.FL:
+        return True
+    if min_qual == Qual.FL and pilot.qual == Qual.IP:
+        return True
+
+    # WG requirements
+    if min_qual == Qual.WG and pilot.qual == Qual.WG:
+        return True
+    if min_qual == Qual.WG and pilot.qual == Qual.FL:
+        return True
+    if min_qual == Qual.WG and pilot.qual == Qual.IP:
+        return True
+
+    return False
