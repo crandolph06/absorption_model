ffdf9dc565cf5d8b2550b509709d10fecb0b3858
Claire Randolph <claire.randolph@hotmail.com>
2026-06-03 21:08:25 -0400

    Updated for 16-output brain including sim targets

--- verify_multi_brain.py
+++ verify_multi_brain.py
@@ -53,10 +53,12 @@ def verify_model():
         'wg_crowding', 'sorties_avail', 'pilot_to_sortie', 'ip_to_stud_ratio',
     ]
     
-    targets = ['wg_monthly', 'fl_monthly', 'ip_monthly', 
+    targets = ['wg_monthly', 'fl_monthly', 'ip_monthly',
                'wg_blue_monthly', 'fl_blue_monthly', 'ip_blue_monthly',
+               'mqt_sim_monthly', 'wg_sim_monthly', 'fl_sim_monthly', 'ip_sim_monthly',
                'remaining_mqt_syllabi_mean', 'remaining_flug_syllabi_mean', 'remaining_ipug_syllabi_mean',
-               'remaining_mqt_syllabi_sorties_only_mean', 'remaining_flug_syllabi_sorties_only_mean', 'remaining_ipug_syllabi_sorties_only_mean',]
+               'remaining_mqt_syllabi_sorties_only_mean', 'remaining_flug_syllabi_sorties_only_mean',
+               'remaining_ipug_syllabi_sorties_only_mean',]
     
     X = df[features].fillna(0)
     
