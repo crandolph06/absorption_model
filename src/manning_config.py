7c051f3488e8b16bef3ca6e0dd9bcb7b314c63da
Claire Randolph <claire.randolph@hotmail.com>
2026-06-10 12:34:35 -0400

    Updated seeding logic to include upgradees for test/validation purposes

--- src/manning_config.py
+++ src/manning_config.py
@@ -6,19 +6,23 @@ FL_YEAR_RANGE = (2016, 2024); FL_HOUR_RANGE = (200, 500);  FL_SORTIE_RANGE = (15
 WG_YEAR_RANGE = (2024, 2026); WG_HOUR_RANGE = (50, 350);   WG_SORTIE_RANGE = (50, 325)
 
 SQUADRON_DATA = [
-    (14, 18, 7, 27), (493, 24, 9, 36), (495, 24, 9, 36), (95, 24, 9, 36),
-    (355, 24, 9, 36), (356, 24, 9, 36), (4, 24, 9, 36), (34, 24, 9, 36),
-    (421, 24, 9, 36), (27, 24, 9, 36), (94, 24, 9, 36), (90, 24, 9, 36),
-    (525, 24, 9, 36), (35, 18, 8, 27), (80, 18, 8, 27), (55, 21, 8, 32),
-    (77, 21, 8, 32), (79, 21, 8, 32), (510, 20, 8, 30), (555, 20, 8, 30),
-    (13, 18, 7, 27), (36, 21, 8, 32), (480, 23, 9, 35), (18, 18, 7, 27),
-    (335, 21, 8, 32), (336, 21, 8, 32), (492, 21, 8, 32), (494, 21, 8, 32),
-    (389, 18, 7, 27), (391, 24, 9, 36)
+    (14, 18, 7, 27, ()), (493, 24, 9, 36, ()), (495, 24, 9, 36, ()), (95, 24, 9, 36, ()),
+    (355, 24, 9, 36, ()), (356, 24, 9, 36, ()), (4, 24, 9, 36, ()), (34, 24, 9, 36, ()),
+    (421, 24, 9, 36, ()), (27, 24, 9, 36, ()), (94, 24, 9, 36, ()), (90, 24, 9, 36, ()),
+    (525, 24, 9, 36, ()), (35, 18, 8, 27, ()), (80, 18, 8, 27, ()), (55, 21, 8, 32, ()),
+    (77, 21, 8, 32, ()), (79, 21, 8, 32, ()), (510, 20, 8, 30, ()), (555, 20, 8, 30, ()),
+    (13, 18, 7, 27, ()), (36, 21, 8, 32, ()), (480, 23, 9, 35, ()), (18, 18, 7, 27, ()),
+    (335, 21, 8, 32, ()), (336, 21, 8, 32, ()), (492, 21, 8, 32, ()), (494, 21, 8, 32, ()),
+    (389, 18, 7, 27, ()), (391, 24, 9, 36, ())
 ]
 
-def get_initial_squadrons(current_year: int):
+TEST_SQUADRON_DATA = [
+    (14, 10, 2, 10, (2, 0, 0))#, (493, 10, 2, 10), (495, 10, 2, 10), (95, 10, 2, 10) # All units 10 PAA, 2 IPs, 10 pilots total
+]
+
+def get_initial_squadrons(current_year: int, squadron_data: list):
     squadrons = []
-    for sq_id, paa, ip_qty, target_total in SQUADRON_DATA:
+    for sq_id, paa, ip_qty, target_total, upgradees in squadron_data:
         sq = SquadronConfig(id=sq_id, paa=paa, ute=10.0, ip_qty=ip_qty, pilots=[])
         
         # 1. Seed IPs
@@ -42,6 +46,30 @@ def get_initial_squadrons(current_year: int):
             sq.pilots.append(Pilot(qual=Qual.WG, year_group=yg, adsc_remaining=max(0, 120-((current_year-yg-2)*12)), 
                              sorties_flown=random.randint(*WG_SORTIE_RANGE), flight_hours_flown=random.randint(*WG_HOUR_RANGE), 
                              squadron_id=sq_id, current_assignment=Assignment.LINE, active=True))
+        
+        # 4. Seed upgrades within current manning
+        for mqt, flug, ipug in upgradees:
+            if mqt > 0: # Default to youngest wingmen
+                mqt_eligible_wingmen = [p for p in sq.pilots if p.qual == Qual.WG] 
+                mqt_eligible_wingmen.sort(key=lambda x: x.year_group)
+                if len(mqt_eligible_wingmen) < mqt:
+                    raise ValueError(f"Not enough wingmen eligible for MQT upgrades: {len(mqt_eligible_wingmen)} available, {mqt} requested")
+                for i in range(mqt):
+                    mqt_eligible_wingmen[i].upgrade = Upgrade.MQT
+            if flug > 0: # Default to oldest wingmen
+                flug_eligible_wingmen = [p for p in sq.pilots if p.qual == Qual.WG and p.upgrade == Upgrade.NONE] 
+                if len(flug_eligible_wingmen) < flug:
+                    raise ValueError(f"Not enough wingmen eligible for FLUG upgrades: {len(flug_eligible_wingmen)} available, {flug} requested")
+                flug_eligible_wingmen.sort(key=lambda x: x.year_group, reverse=True)
+                for i in range(flug):
+                    flug_eligible_wingmen[i].upgrade = Upgrade.FLUG
+            if ipug > 0: # Default to oldest flight leads
+                ipug_eligible_flight_leads = [p for p in sq.pilots if p.qual == Qual.FL] 
+                if len(ipug_eligible_flight_leads) < ipug:
+                    raise ValueError(f"Not enough flight leads eligible for IPUG upgrades: {len(ipug_eligible_flight_leads)} available, {ipug} requested")
+                ipug_eligible_flight_leads.sort(key=lambda x: x.year_group, reverse=True)
+                for i in range(ipug):
+                    ipug_eligible_flight_leads[i].upgrade = Upgrade.IPUG
         
         sq.update_stats()
         squadrons.append(sq)
