b2d6af5e179ebee2264415a568310621ecc5c590
Claire Randolph <claire.randolph@hotmail.com>
2026-06-10 19:30:32 -0400

    Initial commit updated for single phase length days field

--- src/simulation_config.py
+++ src/simulation_config.py
@@ -10,8 +10,7 @@ DEBUG_PHASE_LENGTH_DAYS = 30
 class SimulationConfig:
     """Fleet-wide simulation settings; shared by all squadrons in a run."""
 
-    # phase_length_days: int = DEFAULT_PHASE_LENGTH_DAYS
-    phase_length_days_debug: int = DEBUG_PHASE_LENGTH_DAYS
+    phase_length_days: int = DEFAULT_PHASE_LENGTH_DAYS
     allocation_noise: float = 0.0
 
     @property
